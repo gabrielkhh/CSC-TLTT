b5a4c019a6a8368d889859efb663ed948e6445a1
unknown <gabrielkok4227@gmail.com>
2017-07-25 16:36:23 +0800

    Initial Commit

---
+++ TheLifeTimeTalents/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheLifeTimeTalents.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}

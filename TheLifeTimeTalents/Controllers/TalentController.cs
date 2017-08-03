5e0e1c061009e7dc650c18fdddd3b488f31a4f51
Jewis <jewis@4newperspective.com>
2017-08-03 13:59:17 +0800

    Added EditTalent.cshtml

--- TheLifeTimeTalents/Controllers/TalentController.cs
+++ TheLifeTimeTalents/Controllers/TalentController.cs
@@ -20,5 +20,11 @@ namespace TheLifeTimeTalents.Controllers
         {
             return View();
         }
+
+        public IActionResult EditTalent()
+        {
+            return View();
+        }
+
     }
 }

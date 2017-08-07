9bb593375ac3a4c606b4c568cdb7efdd9480c5c7
Jewis <jewis@4newperspective.com>
2017-08-07 17:47:15 +0800

    Able to view add and update talent now

--- TheLifeTimeTalents/wwwroot/js/site.js
+++ TheLifeTimeTalents/wwwroot/js/site.js
@@ -1,7 +1,1 @@
 ﻿// Write your Javascript code.
-//AWS.config.update({
-//    region: "us-west-2",
-//    endpoint: 'apigateway.us-west-2.amazonaws.com',
-//    accessKeyId: "AKIAI6R6STURXVOILDOA",
-//    secretAccessKey: "PYMb25pvcEcGMXAE4Wz/Beiq3qXDgWdov7LdVgHE"
-//});

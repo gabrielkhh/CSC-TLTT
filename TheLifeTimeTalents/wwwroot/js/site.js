d922ecf0cdcac30576871d72e39fad558feb8b80
Jewis <jewis@4newperspective.com>
2017-08-07 14:04:07 +0800

    Added Fileinput string

--- TheLifeTimeTalents/wwwroot/js/site.js
+++ TheLifeTimeTalents/wwwroot/js/site.js
@@ -1,1 +1,7 @@
 ﻿// Write your Javascript code.
+AWS.config.update({
+    region: "us-west-2",
+    endpoint: 'apigateway.us-west-2.amazonaws.com',
+    accessKeyId: "AKIAI6R6STURXVOILDOA",
+    secretAccessKey: "PYMb25pvcEcGMXAE4Wz/Beiq3qXDgWdov7LdVgHE"
+});
